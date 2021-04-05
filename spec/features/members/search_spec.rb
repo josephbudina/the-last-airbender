a6c1f21d5f619a702b35501a8cdbc850fb07b2eb
Joseph Budina <joebud3@gmail.com>
2021-04-05 10:38:36 -0600

    Add expect for view and character_count to view

--- spec/features/members/search_spec.rb
+++ spec/features/members/search_spec.rb
@@ -11,7 +11,7 @@ describe 'Air Bender Api' do
 
     expect(current_path).to eq(search_path)
     # Then I should see the total number of people who live in the Fire Nation. (should be close to 100)
-    expect(page).to have_content('0')
+    expect(page).to have_content('20')
     # And I should see a list with the detailed information for the first 25 members of the Fire Nation.
     # And for each of the members I should see:
     # - The name of the member (and their photo, if they have one)
